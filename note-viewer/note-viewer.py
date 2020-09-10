bdc2e5b7075a49736e6e206a50bde90eb20ffd2b
Ivy <ivyjs@hendrix.edu>
2020-09-10 11:30:45 -0500

    Started work on note viewer for development
    Started work on a debugging note viewer. This isn't for use in the final app, but it will be highly useful in transforming the text files into images that I can see, and it will give me insights into how the structure of the data works.

---
+++ note-viewer/note-viewer.py
@@ -0,0 +1,46 @@
+import io
+
+class Note:
+    name = "Unnamed Note"
+    lines = []
+    def __init__(self, filename):
+        with open(filename) as f:
+            self.name = f.readline()
+            print(f"Name: {self.name}")
+            lines = f.read().split("\n")
+            self.lines = self.turn_text_into_points(lines)
+            
+            print(self.lines[0].point_1.x)
+            
+    def turn_text_into_points(self,text_lines):
+        lines = []
+        for line_of_text in text_lines:
+            splitted_by_semicolon = line_of_text.split(";")
+            splitted_by_semicolon.remove('')
+
+            for i in range(0,len(splitted_by_semicolon)-1,2):
+                current_pair = splitted_by_semicolon[i].split(",")
+                next_pair = splitted_by_semicolon[i+1].split(",")
+                line = Line(Point(current_pair[0],current_pair[1]), Point(next_pair[0],next_pair[1]))
+
+                lines.append(line)
+
+        return lines
+
+class Point:
+    x=-1
+    y=-1
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
+class Line:
+    point_1 = Point(-1,-1)
+    point_2 = Point(-1,-1)
+
+    def __init__(self, point_1, point_2):
+        self.point_1=point_1
+        self.point_2=point_2
+
+
+
