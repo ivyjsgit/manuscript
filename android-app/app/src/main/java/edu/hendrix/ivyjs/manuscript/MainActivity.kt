5131a901151a330f936785f58daf6505ce14b557
Ivy <ivyjs@hendrix.edu>
2020-09-30 14:28:49 -0500

    Added symbol images, attempt to try to draw

--- android-app/app/src/main/java/edu/hendrix/ivyjs/manuscript/MainActivity.kt
+++ android-app/app/src/main/java/edu/hendrix/ivyjs/manuscript/MainActivity.kt
@@ -1,14 +1,32 @@
 package edu.hendrix.ivyjs.manuscript
 
+import android.app.Application
+import android.content.Context
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         var symbol =  Symbol("6-8-Time",  "A");
-        println(symbol.symbolType);
+//        println(symbol.symbolType);
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val context: Context = MainActivity@this.applicationContext
+        val symbolImage = symbol.getSymbolDrawable(context);
+        val canvas = Canvas();
+
+        when(symbolImage){
+            null -> {
+                Log.i("Manuscript:", "SymbolImage is null")
+            }
+            else -> {
+                Log.i("Manuscript:","SymbolImage is an image!");
+                symbolImage.draw(canvas);
+            }
+        }
     }
 }
+
--- android-app/app/src/main/java/edu/hendrix/ivyjs/manuscript/Symbol.kt
+++ android-app/app/src/main/java/edu/hendrix/ivyjs/manuscript/Symbol.kt
@@ -1,4 +1,7 @@
 package edu.hendrix.ivyjs.manuscript
+import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.core.content.res.ResourcesCompat
 import edu.hendrix.ivyjs.manuscript.SymbolEnum.*;
 import edu.hendrix.ivyjs.manuscript.SymbolPosition.*;
 
@@ -48,7 +51,47 @@ class Symbol(symbolType: String, symbolPositionOnStaff: String) {
             "Whole-Note" -> WHOLENOTE
 
             else -> UNKNOWN
+        }
+    }
 
+    fun getSymbolDrawable(context: Context): Drawable? {
+        return when (this.symbolType){
+
+                TWELVEEIGHTTIME-> ResourcesCompat.getDrawable(context.resources, R.drawable.twelve_eight_time, null)
+                TWOTWOTIME-> ResourcesCompat.getDrawable(context.resources, R.drawable.two_two_time, null)
+                TWOFOURTIME->ResourcesCompat.getDrawable(context.resources, R.drawable.two_four_time, null)
+                THREEFOUR->ResourcesCompat.getDrawable(context.resources, R.drawable.three_four_time, null)
+                THREEEIGHTTIME->ResourcesCompat.getDrawable(context.resources, R.drawable.three_eight_time, null)
+                FOURFOURTIME->ResourcesCompat.getDrawable(context.resources, R.drawable.four_four_time, null)
+                SIXEIGHTTIME->ResourcesCompat.getDrawable(context.resources, R.drawable.six_eight_time, null)
+                NINEEIGHTTIME->ResourcesCompat.getDrawable(context.resources, R.drawable.nine_eight_time, null)
+                BARLINE->ResourcesCompat.getDrawable(context.resources, R.drawable.barline, null)
+                CCLEF->ResourcesCompat.getDrawable(context.resources, R.drawable.c_clef, null)
+                COMMONTIME->ResourcesCompat.getDrawable(context.resources, R.drawable.common_time, null)
+                CUTTIME->ResourcesCompat.getDrawable(context.resources, R.drawable.cut_time, null)
+                DOT->ResourcesCompat.getDrawable(context.resources, R.drawable.dot, null)
+                DOUBLESHARP->ResourcesCompat.getDrawable(context.resources, R.drawable.double_sharp, null)
+                EIGHTHNOTE->ResourcesCompat.getDrawable(context.resources, R.drawable.eighth_note, null)
+                EIGHTHREST->ResourcesCompat.getDrawable(context.resources, R.drawable.eighth_rest, null)
+                FCLEF-> ResourcesCompat.getDrawable(context.resources, R.drawable.f_clef, null)
+                FLAT->ResourcesCompat.getDrawable(context.resources, R.drawable.flat, null)
+                GCLEF->ResourcesCompat.getDrawable(context.resources, R.drawable.g_clef, null)
+                HALFNOTE->ResourcesCompat.getDrawable(context.resources, R.drawable.half_note, null)
+                NATURAL->ResourcesCompat.getDrawable(context.resources, R.drawable.natural, null)
+                QUARTERNOTE->ResourcesCompat.getDrawable(context.resources, R.drawable.quarter_note, null)
+                QUARTERREST->ResourcesCompat.getDrawable(context.resources, R.drawable.quarter_rest, null)
+                SHARP->ResourcesCompat.getDrawable(context.resources, R.drawable.sharp, null)
+                SIXTEENTHNOTE->ResourcesCompat.getDrawable(context.resources, R.drawable.sixteenth_note, null)
+                SIXTEENTHREST->ResourcesCompat.getDrawable(context.resources, R.drawable.sixteenth_rest, null)
+                SIXTYFOURNOTE->ResourcesCompat.getDrawable(context.resources, R.drawable.sixty_four_note, null)
+                SIXTYFOURREST->ResourcesCompat.getDrawable(context.resources, R.drawable.sixty_four_rest, null)
+                THIRTYTWONOTE->ResourcesCompat.getDrawable(context.resources, R.drawable.thirty_two_note, null)
+                THIRTYTWOREST->ResourcesCompat.getDrawable(context.resources, R.drawable.thirty_two_rest, null)
+                WHOLEHALFREST->ResourcesCompat.getDrawable(context.resources, R.drawable.whole_half_rest, null)
+                WHOLENOTE->ResourcesCompat.getDrawable(context.resources, R.drawable.whole_note, null)
+                UNKNOWN->null
+
+            else -> null
         }
     }
 }
